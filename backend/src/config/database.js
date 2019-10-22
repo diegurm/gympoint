faa3f29681a9d7253646ad43ca8e7ed035fe273e
Diego Rodrigues <diegurm@hotmail.com>
2019-10-21 22:12:15 -0300

    :zap: Criado conexão  com o banco de dados e migrate do usuario

---
+++ backend/src/config/database.js
@@ -0,0 +1,19 @@
+const dotenv = require('dotenv');
+
+dotenv.config({
+  path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env.development',
+});
+
+module.exports = {
+  dialect: 'mysql',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT || '3306'),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoreAll: true,
+  },
+};
