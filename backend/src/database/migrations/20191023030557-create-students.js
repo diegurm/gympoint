d9ffafb88a4c73c5dc15b4d9e6bedb5f6fe663d9
Diego Rodrigues <diegurm@hotmail.com>
2019-10-23 01:19:38 -0300

    :zap: Atualizado migrate de students adicionando colunas de updated_at/created_at

--- backend/src/database/migrations/20191023030557-create-students.js
+++ backend/src/database/migrations/20191023030557-create-students.js
@@ -27,6 +27,14 @@ module.exports = {
         type: Sequelize.DOUBLE,
         allowNull: false,
       },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
     });
   },
 
