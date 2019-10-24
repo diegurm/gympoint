bf3604bfa093de263122b84ea9465a6511d142a5
Diego Rodrigues <diegurm@hotmail.com>
2019-10-23 22:39:48 -0300

    :sparkles: Adicionado método de atualizar do StudentController

--- backend/src/app/controllers/StudentController.js
+++ backend/src/app/controllers/StudentController.js
@@ -32,6 +32,50 @@ class StudentController {
     const student = await Student.create(req.body);
     return res.json({ student });
   }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required('Name is required'),
+      email: Yup.string().required('Email is required'),
+      age: Yup.number()
+        .typeError('Age must be a number')
+        .positive('Age must be greater than zero')
+        .required('Age is required'),
+      wheigth: Yup.number()
+        .positive('Wheigth must be greater than zero')
+        .required('Wheigth is required'),
+      height: Yup.number()
+        .positive('Heigth must be greater than zero')
+        .required('Heigth is required'),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { id } = req.params;
+    const { email } = req.body;
+
+    const student = await Student.findByPk(id);
+    if (!student) {
+      return res.status(400).json({ error: 'Student not found' });
+    }
+
+    if (email !== student.email) {
+      const studentExists = await Student.findOne({ where: { email } });
+      if (studentExists) {
+        return res.status(400).json({ error: 'Student already exists' });
+      }
+    }
+    const { name, wheigth, height } = await student.update(req.body);
+
+    return res.json({
+      name,
+      email,
+      wheigth,
+      height,
+    });
+  }
 }
 
 export default new StudentController();
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -12,5 +12,6 @@ routes.post('/sessions', StoreController.store);
 routes.use(authMiddleware);
 
 routes.post('/students', StudentController.store);
+routes.put('/students/:id', StudentController.update);
 
 export default routes;
