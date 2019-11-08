6f1f2c204c2eb980d68b587a0bc0c8b044a47c27
Diego Rodrigues <diegurm@hotmail.com>
2019-11-08 11:57:44 -0300

    :sparkles: Implementado método update no PlanController

--- backend/src/app/controllers/PlanController.js
+++ backend/src/app/controllers/PlanController.js
@@ -23,6 +23,33 @@ class PlanController {
 
     res.json(plan);
   }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string().required('Title is required'),
+      duration: Yup.number()
+        .typeError('Duration must be a number')
+        .positive('Duration must be greater than zero')
+        .required('Duration is required'),
+      price: Yup.number()
+        .typeError('Price must be a number')
+        .positive('Price must be greater than zero')
+        .required('Price is required'),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const plan = await Plan.findByPk(req.params.id);
+    if (!plan) {
+      return res.status(400).json({ error: 'Plan not found' });
+    }
+
+    await plan.update(req.body);
+
+    res.json(plan);
+  }
 }
 
 export default new PlanController();
