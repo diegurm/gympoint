9aaeb7d5f248890baa4cf8b1ebce0c12787c8066
Diego Rodrigues <diegurm@hotmail.com>
2019-12-05 22:49:26 -0300

    :sparkles: Implementado update no EnrollmentController

--- backend/src/app/controllers/EnrollmentController.js
+++ backend/src/app/controllers/EnrollmentController.js
@@ -78,6 +78,56 @@ class EnrollmentController {
     return res.json(enrollment);
   }
 
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      student_id: Yup.number().required('Student is required'),
+      plan_id: Yup.number().required('Plan is required'),
+      start_date: Yup.date().required('Start date is required'),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+    const { id } = req.params;
+    const { student_id, plan_id, start_date } = req.body;
+
+    if(isPast(parseISO(start_date))){
+      return res.status(400).json({ error: 'Start date has passed' });
+    }
+
+    const enrollment = await Enrollment.findByPk(id);
+    if (!enrollment) {
+      return res.status(400).json({ error: 'Enrollment not found' });
+    }
+
+    const plan = await Plan.findByPk(plan_id);
+    if (!plan) {
+      return res.status(400).json({ error: 'Plan does not exist' });
+    }
+    const end_date = start_date != enrollment.start_date ? addDays(parseISO(start_date), plan.duration * 30) : enrollment.start_date;
+    const price = end_date != enrollment.end_date ? plan.price * plan.duration : enrollment.price;
+
+
+    //Validate enrollment
+    // Validar se a data de inicio coincide com algum periodo ja selecionado anteriormente
+    const enrollmentIsValid = await Enrollment.findOne({
+      where: { student_id },
+      order: [['end_date','DESC']]
+    })
+    if(enrollmentIsValid && enrollmentIsValid.id !== id){
+      if(parseISO(start_date) < enrollmentIsValid.end_date){
+        return res.status(400).json({ error: 'Enrollment already exists for selected period' });
+      }
+    }
+    await enrollment.update({
+      ...req.body,
+      end_date,
+      price,
+    });
+
+    return res.json(enrollment);
+  }
+
   async delete(req, res) {
     const enrollment = await Enrollment.findByPk(req.params.id);
     if (!enrollment) {
