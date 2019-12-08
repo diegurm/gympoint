438a8239ca791161c1b3b7e73bf4aea4ed47da7f
Diego Rodrigues <diegurm@hotmail.com>
2019-12-08 13:54:42 -0300

    :sparkles: Criado migration para a tabela de checkins

--- backend/src/database/migrations/20191208032338-chekins.js
+++ backend/src/database/migrations/20191208032338-chekins.js
@@ -1,6 +1,6 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('enrollments', {
+    return queryInterface.createTable('checkins', {
       id: {
         type: Sequelize.INTEGER,
         allowNull: false,
@@ -11,26 +11,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         references: { model: 'students', key: 'id' },
         onUpdate: 'CASCADE',
-        onDelete: 'SET NULL',
-        allowNull: true,
-      },
-      plan_id: {
-        type: Sequelize.INTEGER,
-        references: { model: 'plans', key: 'id' },
-        onUpdate: 'CASCADE',
-        onDelete: 'SET NULL',
-        allowNull: true,
-      },
-      start_date: {
-        type: Sequelize.DATE,
-        allowNull: false,
-      },
-      end_date: {
-        type: Sequelize.DATE,
-        allowNull: false,
-      },
-      price: {
-        type: Sequelize.DECIMAL,
+        onDelete: 'CASCADE',
         allowNull: false,
       },
       created_at: {
@@ -45,6 +26,6 @@ module.exports = {
   },
 
   down: queryInterface => {
-    return queryInterface.dropTable('enrollments');
+    return queryInterface.dropTable('checkins');
   },
 };
